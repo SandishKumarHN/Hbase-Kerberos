f7a1a77341d4928b3361f62b9ebbfde063e0d7ef
sanysandish@gmail.com <sany@Brocks-MacBook-Pro.local>
2017-02-06 09:37:51 +0530

    initial commit

---
+++ src/main/java/com/ir/hbase/App/App.java
@@ -0,0 +1,45 @@
+package com.ir.hbase.App;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.security.UserGroupInformation;
+import java.io.IOException;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        System.out.println( "Hello World!" );
+
+        Configuration config = HBaseConfiguration.create();
+        UserGroupInformation.setConfiguration(config);
+        UserGroupInformation.loginUserFromKeytab("cccdih@domainname.com", "/home/username/username.keytab");
+        
+        HTable table = new HTable(config, "emp");
+
+        // Instantiating Get class
+        Get g = new Get(Bytes.toBytes("row1"));
+
+        // Reading the data
+        Result result = table.get(g);
+
+        // Reading values from Result class object
+        byte [] value = result.getValue(Bytes.toBytes("personal"),Bytes.toBytes("name"));
+
+        byte [] value1 = result.getValue(Bytes.toBytes("personal"),Bytes.toBytes("city"));
+
+        // Printing the values
+        String name = Bytes.toString(value);
+        String city = Bytes.toString(value1);
+        
+        System.out.println("name: " + name + " city: " + city);
+    }
+}
